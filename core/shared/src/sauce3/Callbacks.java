0eebae5fa3226b49d9ea0a713813e867bd749163
Lord Niels N. Horn <i.am@nilq.dk>
2016-11-11 15:11:25 +0100

    Implement more core features

--- core/shared/src/sauce3/Callbacks.java
+++ core/shared/src/sauce3/Callbacks.java
@@ -28,69 +28,81 @@ public class Callbacks {
     }
 
     public void resize(int width, int height) {
-        runCallback("resize", width, height);
+    run_callback("resize", width, height);
+  }
+
+  public void visible(boolean isVisible) {
+    run_callback("visible", isVisible);
+  }
+
+  public void quit() {
+    run_callback("_quit");
+  }
+
+  public void keypressed(int keycode) {
+    run_callback("_keypressed", keycode);
+  }
+
+  public void keyreleased(int keycode) {
+    run_callback("_keyreleased", keycode);
+  }
+
+  public void textinput(String character) {
+    run_callback("textinput", character);
+  }
+
+  public void touchpressed(int x, int y, int pointer) {
+    run_callback("touchpressed", x, y, pointer);
+  }
+
+  public void mousepressed(int x, int y, int buttoncode) {
+    run_callback("_mousepressed", x, y, buttoncode);
+  }
+
+  public void touchreleased(int x, int y, int pointer) {
+    run_callback("touchreleased", x, y, pointer);
+  }
+
+  public void mousereleased(int x, int y, int buttoncode) {
+    run_callback("_mousereleased", x, y, buttoncode);
+  }
+
+  public void touchmoved(int x, int y, int pointer) {
+    run_callback("touchmoved", x, y, pointer);
+  }
+
+  public void mousemoved(int x, int y) {
+    run_callback("mousemoved", x, y);
+  }
+
+  public void mousescrolled(int amount) {
+    run_callback("mousescrolled", amount);
+  }
+
+  private void run_callback(String key, Object... args) {
+    if (!is_enabled || vm.lua == null) {
+      return;
     }
 
-    public void visible(boolean isVisible) {
-        runCallback("visible", isVisible);
+    LuaValue callback = root.get(key);
+
+    if (!callback.isfunction()) {
+      return;
     }
 
-    public void quit() {
-        runCallback("_quit");
+    switch (args.length) {
+      case 0:
+        callback.call();
+        break;
+      case 1:
+        callback.call(CoerceJavaToLua.coerce(args[0]));
+        break;
+      case 2:
+        callback.call(CoerceJavaToLua.coerce(args[0]), CoerceJavaToLua.coerce(args[1]));
+        break;
+      case 3:
+        callback.call(CoerceJavaToLua.coerce(args[0]), CoerceJavaToLua.coerce(args[1]), CoerceJavaToLua.coerce(args[2]));
+        break;
     }
-
-    public void keypressed(int keycode) {
-        runCallback("_keypressed", keycode);
-    }
-
-    public void keyreleased(int keycode) {
-        runCallback("_keyreleased", keycode);
-    }
-
-    public void textinput(String character) {
-        runCallback("textinput", character);
-    }
-
-    public void touchpressed(int x, int y, int pointer) {
-        runCallback("touchpressed", x, y, pointer);
-    }
-
-    public void mousepressed(int x, int y, int buttoncode) {
-        runCallback("_mousepressed", x, y, buttoncode);
-    }
-
-    public void touchreleased(int x, int y, int pointer) {
-        runCallback("touchreleased", x, y, pointer);
-    }
-
-    public void mousereleased(int x, int y, int buttoncode) {
-        runCallback("_mousereleased", x, y, buttoncode);
-    }
-
-    public void touchmoved(int x, int y, int pointer) {
-        runCallback("touchmoved", x, y, pointer);
-    }
-
-    public void mousemoved(int x, int y) {
-        runCallback("mousemoved", x, y);
-    }
-
-    public void mousescrolled(int amount) {
-        runCallback("mousescrolled", amount);
-    }
-
-    private void runCallback(String name, Object... args) {
-        if (!enabled) return;
-        if (vm.lua == null) return;
-
-        LuaValue callback = root.get(name);
-        if (!callback.isfunction()) return;
-
-        switch (args.length) {
-            case 0: callback.call(); break;
-            case 1: callback.call(CoerceJavaToLua.coerce(args[0])); break;
-            case 2: callback.call(CoerceJavaToLua.coerce(args[0]), CoerceJavaToLua.coerce(args[1])); break;
-            case 3: callback.call(CoerceJavaToLua.coerce(args[0]), CoerceJavaToLua.coerce(args[1]), CoerceJavaToLua.coerce(args[2])); break;
-        }
-    }
+}
 }
--- src/sauce3.moon
+++ src/sauce3.moon
@@ -18,6 +18,28 @@ sauce3.Quad    = require "sauce3/utils/Quad"
 sauce3.file_system = require "sauce3/core/file_system"
 sauce3.timer       = require "sauce3/core/timer"
 sauce3.graphics    = require "sauce3/core/graphics"
+sauce3.console     = require "sauce3/core/console"
+sauce3.system      = require "sauce3/core/system"
+sauce3.timer       = require "sauce3/core/timer"
+
+Constants = require "sauce3/wrappers"
+import key_codes, button_codes from Constants
+
+sauce3._keypressed = (key) ->
+  (sauce3.key_pressed key_codes[key]) if sauce3.keypressed
+
+sauce3._keyreleased = (key) ->
+  (sauce3.key_released key_codes[key]) if sauce3.keyreleased
+
+sauce3._mousepressed = (x, y, button) ->
+  (sauce3.mouse_pressed button_codes[button]) if sauce3.mouse_pressed
+
+sauce3._mousereleased = (x, y, button) ->
+  (sauce3.mouse_released button_codes[button]) if sauce3.mouse_released
+
+sauce3._quit = ->
+  if sauce3.quit then sauce3.quit!
+  sauce3.audio.stop_all!
 
 sauce3.run = ->
   dt = sauce3.timer.get_delta!
